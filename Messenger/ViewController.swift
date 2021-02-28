c299a867dd1d6c93644899289b5fe26565432f1d
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-02-28 19:02:21 +0700

    first commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Uwais Alqadri on 28/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
