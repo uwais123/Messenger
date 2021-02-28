0716a30566195e979b64d068242cafb462cb3cd5
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-02-28 19:14:26 +0700

    group files

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversationsViewController.swift
+//  Messenger
+//
+//  Created by Uwais Alqadri on 28/02/21.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Messenger/Controllers/LoginViewController.swift
+++ Messenger/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Messenger
 //
 //  Created by Uwais Alqadri on 28/02/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
